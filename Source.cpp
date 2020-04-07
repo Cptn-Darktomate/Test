e10833d765061d8a7950531d3fca9658bb207532
Cptn-Darktomate <63303560+Cptn-Darktomate@users.noreply.github.com>
2020-04-07 17:07:43 +0200

    souce.cpp
    test

---
+++ Source.cpp
@@ -0,0 +1,31 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>;
+
+
+
+int main()
+{
+	sf::RenderWindow MainScreen(sf::VideoMode(1080, 960, 32), "Generic SFML FILE");
+
+	sf::Event evnts;
+
+	while (MainScreen.isOpen())
+	{
+
+		while (MainScreen.pollEvent(evnts))
+		{
+			switch (evnts.type)
+			{
+			case sf::Event::Closed:
+				MainScreen.close();
+			default:
+				break;
+			}
+		}
+
+	}
+
+	return 0;
+}
